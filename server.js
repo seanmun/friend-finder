862000a8e79ea8fac6b9b99fb85ee897214030b7
seanmun <smunley13@gmail.com>
2019-04-29 23:30:35 -0400

    assets

---
+++ server.js
@@ -0,0 +1,92 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Star Wars Tables (DATA)
+// =============================================================
+
+var friends = [
+    {
+    name:"Sean",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ],
+    },
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/survey.html"));
+});
+
+
+// Displays all friends
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
+
+
+// // Displays a single friends, or returns false
+app.get("/api/friends/:friend", function(req, res) {
+  var chosen = req.params.friend;
+
+  console.log(chosen);
+
+  for (var i = 0; i < friends.length; i++) {
+    if (chosen === friends[i].routeName) {
+      return res.json(friends[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New friends - takes in JSON input
+app.post("/api/friends", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var userData = req.body;
+
+  // Using a RegEx Pattern to remove spaces from userData
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  userData.routeName = userData.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(userData);
+
+    friends.push(userData);
+  
+  res.json(userData);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
